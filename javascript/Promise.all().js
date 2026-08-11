08056f7a848a84e7eeae45cdfd384fa5ccd7ee8e
Aryan <lnegi8596@gmail.com>
2026-08-11 22:15:06 +0530

    Update Promise.all().js

--- javascript/Promise.all().js
+++ javascript/Promise.all().js
@@ -1,11 +1,26 @@
 const p1 = Promise.resolve("HTML");
-const p2 = Promise.resolve("CSS");
-const p3 = Promise.resolve("JavaScript");
+// Promise.resolve() ek fulfilled Promise banata hai
+// Is Promise ki value "HTML" hai
 
+const p2 = Promise.resolve("CSS");
+// Ek fulfilled Promise banaya
+// Iski value "CSS" hai
+
+const p3 = Promise.resolve("JavaScript");
+// Ek fulfilled Promise banaya
+// Iski value "JavaScript" hai
+
+
+// Promise.all() teeno Promises ke complete hone ka wait karta hai
 Promise.all([p1, p2, p3])
-.then((result) => {
-    console.log(result);
-})
-.catch((error) => {
-    console.log(error);
-});
+
+    .then((result) => {
+        // Teeno Promises ki values ek array mein milengi
+        console.log(result);
+    })
+
+    .catch((error) => {
+        // Agar koi Promise reject/fail ho jaye,
+        // to catch() error ko handle karega
+        console.log(error);
+    });
