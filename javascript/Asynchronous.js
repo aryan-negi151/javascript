e03033248cc16e0373b4d5c3cd771bb12d3e5b2a
Aryan <lnegi8596@gmail.com>
2026-08-11 09:22:56 +0530

    Enhance Asynchronous.js with explanatory comments
    Added comments to explain asynchronous behavior of setTimeout.

--- javascript/Asynchronous.js
+++ javascript/Asynchronous.js
@@ -1,7 +1,13 @@
+// Print "Start" immediately
 console.log("Start");
 
+// setTimeout() runs the function after 3 seconds (3000 milliseconds)
+// JavaScript does not wait here; it continues executing the next line
 setTimeout(() => {
+    // This message will be printed after 3 seconds
     console.log("Task Finished");
 }, 3000);
 
+// Print "End" immediately
+// This runs before "Task Finished" because setTimeout is asynchronous
 console.log("End");
