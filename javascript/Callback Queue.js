4ea931f48a458196d995a1f964b7cf1e6daa9f63
Aryan <lnegi8596@gmail.com>
2026-08-11 09:24:17 +0530

    Enhance Callback Queue.js with comments
    Added comments to explain setTimeout usage.

--- javascript/Callback Queue.js
+++ javascript/Callback Queue.js
@@ -1,3 +1,7 @@
+// setTimeout() is used to execute a function after a certain amount of time
 setTimeout(() => {
+
+    // This message will be printed after 2 seconds
     console.log("Hello");
-}, 2000);
+
+}, 2000); // 2000 milliseconds = 2 seconds
