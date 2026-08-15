d5b42f4a1ee4a225966a20e6f85e1fafb9d0e64e
Aryan <lnegi8596@gmail.com>
2026-08-15 11:33:33 +0530

    Update Promise.any().js

--- javascript/Promise.any().js
+++ javascript/Promise.any().js
@@ -1,15 +1,46 @@
+// Promise 1 banaya
+// Ye immediately reject hoga
 const p1 = Promise.reject("Error 1");
 
+
+// Promise 2 banaya
+// Ye 2 seconds ke baad resolve hoga
 const p2 = new Promise((resolve) => {
-    setTimeout(() => resolve("Success"), 2000);
+
+    // 2000 milliseconds = 2 seconds
+    setTimeout(() => {
+
+        // 2 seconds baad Promise successful hoga
+        resolve("Success");
+
+    }, 2000);
 });
 
+
+// Promise 3 banaya
+// Ye bhi immediately reject hoga
 const p3 = Promise.reject("Error 3");
 
+
+// Promise.any() ka use kiya
+// Ye promises me se jo PEHLA successfully resolve hoga,
+// uska result return karega
 Promise.any([p1, p2, p3])
+
+
+// Jab koi ek Promise successfully resolve ho jayega,
+// .then() execute hoga
 .then((result) => {
+
+    // Successful Promise ki value print hogi
     console.log(result);
 })
+
+
+// Agar SABHI Promises reject ho jayein,
+// tab .catch() execute hoga
 .catch((error) => {
+
+    // Error print hoga
     console.log(error);
 });
