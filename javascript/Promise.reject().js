865f2c96a5a41812db45634a50acae31772bf411
Aryan <lnegi8596@gmail.com>
2026-08-15 11:34:52 +0530

    Enhance Promise.reject() example with comments
    Added comments to explain Promise rejection and catch behavior.

--- javascript/Promise.reject().js
+++ javascript/Promise.reject().js
@@ -1,4 +1,11 @@
+// Promise ko manually reject kiya
+// "Something Went Wrong" error message hai
 Promise.reject("Something Went Wrong")
+
+// Agar Promise reject hota hai,
+// to .catch() automatically execute hota hai
 .catch((error) => {
+
+    // Reject hua hua error yahan "error" variable me milega
     console.log(error);
 });
