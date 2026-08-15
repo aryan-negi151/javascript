523db7faf53b6d93de26c04537b6816f84a91c7b
Aryan <lnegi8596@gmail.com>
2026-08-15 11:36:19 +0530

    Enhance Promises.js with explanatory comments
    Added comments to clarify promise creation and resolution.

--- javascript/Promises.js
+++ javascript/Promises.js
@@ -1,11 +1,18 @@
+// Ek naya Promise create kiya
 let promise = new Promise((resolve, reject) => {
 
+    // Promise ko successfully resolve kar diya
+    // "Success" iska result/value hai
     resolve("Success");
 
 });
 
+
+// Promise successful hone ke baad
+// .then() automatically execute hoga
 promise.then((result) => {
 
+    // Resolved value "result" variable me milegi
     console.log(result);
 
 });
