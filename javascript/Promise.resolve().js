ef6f094bd7280199046d54329be68c5cf5b4f335
Aryan <lnegi8596@gmail.com>
2026-08-15 11:35:25 +0530

    Enhance Promise.resolve() example with comments
    Added comments explaining Promise resolution and its behavior.

--- javascript/Promise.resolve().js
+++ javascript/Promise.resolve().js
@@ -1,4 +1,11 @@
+// Ek Promise ko successfully resolve kiya
+// "Hello JavaScript" is Promise ki value hai
 Promise.resolve("Hello JavaScript")
+
+// Promise successfully resolve hone ke baad
+// .then() automatically execute hoga
 .then((result) => {
+
+    // Resolved value "result" variable me milegi
     console.log(result);
 });
