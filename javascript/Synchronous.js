270707fea0e485d539e09c36b25be0ae4cebba86
Aryan <lnegi8596@gmail.com>
2026-08-15 11:36:49 +0530

    Update Synchronous.js

--- javascript/Synchronous.js
+++ javascript/Synchronous.js
@@ -1,5 +1,11 @@
+// Sabse pehle "Start" console me print hoga
 console.log("Start");
 
+
+// Uske baad ye line execute hogi
+// "Learning JavaScript" console me print hoga
 console.log("Learning JavaScript");
 
+
+// Sabse last me "End" console me print hoga
 console.log("End");
