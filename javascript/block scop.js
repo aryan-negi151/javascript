05fa7ae2bb39d21372963fc82dbeb83d3515a349
Aryan <lnegi8596@gmail.com>
2026-08-22 22:28:37 +0530

    Update block scop.js

--- javascript/block scop.js
+++ javascript/block scop.js
@@ -1,8 +1,14 @@
 {
+    // Create a variable x using let and store the value 10
     let x = 10;
+
+    // Create a constant y using const and store the value 20
+    // The value of a const variable cannot be changed later
     const y = 20;
 
+    // Print the value of x to the console
     console.log(x);
+
+    // Print the value of y to the console
     console.log(y);
 }
-
